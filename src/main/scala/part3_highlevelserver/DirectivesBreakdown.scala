817ff3c0ef095ce10d94862cc1d508536bb6ff81
peterhuynh7 <chuynh7@uic.edu>
2022-04-27 00:11:25 -0500

    14. Directives Breakdown 2

--- src/main/scala/part3_highlevelserver/DirectivesBreakdown.scala
+++ src/main/scala/part3_highlevelserver/DirectivesBreakdown.scala
@@ -1,9 +1,11 @@
 package part3_highlevelserver
 
 import akka.actor.ActorSystem
+import akka.event.LoggingAdapter
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpRequest, StatusCodes}
 import akka.stream.ActorMaterializer
+import com.sun.javafx.scene.control.Logging
 
 object DirectivesBreakdown extends App {
   implicit val system = ActorSystem("DirectivesBreakdown")
@@ -82,6 +84,100 @@ object DirectivesBreakdown extends App {
       }
     }
 
-  Http().bindAndHandle(queryParamExtractionRoute, "localhost", 8080)
+  val extractRequestRoute =
+    path("controlEndpoint") {
+      extractRequest { (httpRequest: HttpRequest) =>
+        extractLog { (log: LoggingAdapter) =>
+          log.info(s"I got the http request: $httpRequest")
+          complete(StatusCodes.OK)
+        }
+      }
+    }
 
+//  Http().bindAndHandle(extractRequestRoute, "localhost", 8080)
+
+  /*
+  Type #3: composite directives
+   */
+
+  val simpleNestedRoute =
+    path("api" / "item") {
+      get {
+        complete(StatusCodes.OK)
+      }
+    }
+
+  val compactSimpleNestedRoute = (path("api" / "item") & get) {
+    complete(StatusCodes.OK)
+  }
+
+  val compactExtractRequestRoute =
+    (path("controlEndpoint") & extractRequest & extractLog) { (request, log) =>
+      log.info(s"I got the http request: $request")
+      complete(StatusCodes.OK)
+    }
+
+  // /about amd /aboutUs
+  val repeatedRoute =
+    path("about") {
+      complete(StatusCodes.OK)
+    } ~
+    path("aboutUs") {
+      complete(StatusCodes.OK)
+    }
+
+  val dryRoute =
+    (path("about") | path("aboutUs")) {
+      complete(StatusCodes.OK)
+    }
+
+  // yourblog.com/42 AND yourlog.com?postId=42
+  val blogByIdRoute =
+    path(IntNumber) { (blogId: Int) =>
+      // complex server logic
+      complete(StatusCodes.OK)
+    }
+
+  val blogByQueryParamRoute =
+    parameter('postId.as[Int]) { (blogPostId: Int) =>
+      // the SAME server logic
+      complete(StatusCodes.OK)
+    }
+
+  val combinedBlogByIdRoute =
+    (path(IntNumber) | parameter('postId.as[Int])) {(blogPostId: Int) =>
+      // the SAME server logic
+      complete(StatusCodes.OK)
+    }
+
+  /*
+  Type #4: "actionable" directives
+   */
+  val completeOkRoute = complete(StatusCodes.OK)
+
+  val failedRoute =
+    path("notSupported") {
+      failWith(new RuntimeException("Unsupported!")) // completes with HTTP 500
+    }
+
+  val routeWithRejection =
+    path("home") {
+      reject
+    } ~
+      path("index") {
+        completeOkRoute
+      }
+
+  /*
+  Exercise: spot the mistake
+   */
+  val getOrPutPath =
+    path("api" / "myEndpoint") {
+      get {
+        completeOkRoute
+      } ~ // missing tilde
+      post {
+        complete(StatusCodes.Forbidden)
+      }
+    }
 }
